cef9b56fcbd30fb897b42bcf79a1f0740629f260
walz0 <aidan.m.walz@gmail.com>
2021-05-12 11:49:45 -0400

    Added updater module

--- updater.js
+++ updater.js
@@ -1,10 +1,20 @@
-var axios = require('axios');
-var fs = require('fs');
+const axios = require('axios');
+const unzipper = require('unzipper');
+const fs = require('fs');
+const mv = require('mv');
 
 
-async function download() {
-    const url = 'https://github.com/walz0/twitter-dm-bot/releases/download/1.0.0/twitter-bot-win.zip';
-    const path = __dirname + '\\file.zip';
+async function update() {
+    let url = '';
+    switch(process.platform) {
+        case "win32":
+            url = 'https://github.com/walz0/twitter-dm-bot/releases/download/1.0.0/twitter-bot-win.zip';
+            break;
+        case "darwin":
+            // url = 'https://github.com/walz0/twitter-dm-bot/releases/download/1.0.0/twitter-bot-win.zip';
+            break;
+    }
+    const path = __dirname + '/bin.zip';
     
     const response = await axios({
         method: 'GET',
@@ -12,18 +22,33 @@ async function download() {
         responseType: 'stream'
     })
 
+    console.log("Downloading files from", url);
     response.data.pipe(fs.createWriteStream(path))
     
     return new Promise((resolve, reject) => {
         response.data.on('end', () => {
-            console.log('Download complete');
+            console.log('Download complete, extracting files...');
+            // Unzip files
+            fs.createReadStream(path)
+                .pipe(unzipper.Extract({path: __dirname + '/.'}))
+                .on('close', () => {
+                    // Move files to . 
+                    mv('./twitter-bot/', '.', {mkdirp: false, clobber: false}, (err) => {
+                        if (err) throw err;
+                        // Delete unnecessary files
+                        fs.unlink('bin.zip', () => {
+                            fs.unlink('twitter-bot', () => {
+                                console.log('Update complete.');
+                            })
+                        })
+                    });
+                });
             resolve();
-        })
-    
+        });
         response.data.on('error', err => {
             reject(err);
         })
     })
 }
 
-download();
+update();
