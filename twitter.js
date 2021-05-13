a35442fe6cbb7fdd3a8b4621b9ed7b7edfa2f45e
walz0 <aidan.m.walz@gmail.com>
2021-05-13 19:23:32 -0400

    Removed vulnerabilities

--- twitter.js
+++ twitter.js
@@ -3,35 +3,55 @@ const OAuth = require('oauth');
 const axios = require('axios');
 require('dotenv').config();
 
-const consumer_key = "zPOFdtE5g0NiAYyosf0DpMpjA";
-const consumer_secret = "WS7SdCZrv5rCPRrODNf7INf8x7Gm8UaF1viYvG3TGiR0Tl9oJG";
-const access_token = "785961919-dnEx1nNMqPeJ9KDjaZOWE13QBqmMcWGZy6aHGb4n";
-const access_token_secret = "fa0XdIgTPUCqG5Hxokatmx8XhZFoHg0fEU3BJw3pToeYw";
-const bearer_token = "AAAAAAAAAAAAAAAAAAAAANJQPgEAAAAAabgxEyjx12dlrHQDuIWQqmncMzU%3DsJGZrnjEOhtWEzDb7OYjhY2VB2mUKgaNgUKLKvex0jJqpCPQFu";
-
 const dm_url = 'https://api.twitter.com/1.1/direct_messages/events/new.json';
 
-axios({
-    method: 'post',
-    url: dm_url,
-    headers: {
-        "Authorization": 'OAuth oauth_consumer_key="hy9z7IBnscMhYoUg2kaxRcwo0",oauth_token="785961919-JseVFIfyIJNjgehnzzNtImuVWOfYpH55C7uN2JvV",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1620937711",oauth_nonce="9CaleX8ErU8",oauth_version="1.0",oauth_signature="tRY26j1wUIN7wt4CZShzyBRpPG0%3D"',
-        "Content-Type": 'application/json'
-    },
-    data: {
-        "event": {
-            "type": "message_create",
-            "message_create": {
-                "target": {
-                    "recipient_id": "785961919"
-                },
-                "message_data": {
-                    "text": "test"
-                }
-            }
-        }
+const NONCE_CHARS= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+              'o','p','q','r','s','t','u','v','w','x','y','z','A','B',
+              'C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+              'Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3',
+              '4','5','6','7','8','9'];
+
+function getNonce (nonceSize) {
+    var result = [];
+    var chars = NONCE_CHARS;
+    var char_pos;
+    var nonce_chars_length = chars.length;
+
+    for (var i = 0; i < nonceSize; i++) {
+        char_pos= Math.floor(Math.random() * nonce_chars_length);
+        result[i]=  chars[char_pos];
     }
-});
+    return result.join('');
+}
+
+function getTimestamp () {
+  return Math.floor( (new Date()).getTime() / 1000 );
+}
+
+console.log(getNonce(11));
+console.log(getTimestamp());
+
+// axios({
+//     method: 'post',
+//     url: dm_url,
+//     headers: {
+//         "Authorization": 'OAuth oauth_consumer_key="hy9z7IBnscMhYoUg2kaxRcwo0",oauth_token="785961919-JseVFIfyIJNjgehnzzNtImuVWOfYpH55C7uN2JvV",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1620937711",oauth_nonce="9CaleX8ErU8",oauth_version="1.0",oauth_signature="tRY26j1wUIN7wt4CZShzyBRpPG0%3D"',
+//         "Content-Type": 'application/json'
+//     },
+//     data: {
+//         "event": {
+//             "type": "message_create",
+//             "message_create": {
+//                 "target": {
+//                     "recipient_id": "785961919"
+//                 },
+//                 "message_data": {
+//                     "text": "test"
+//                 }
+//             }
+//         }
+//     }
+// });
 
 // const oauth = new OAuth.OAuth(
 //     'https://api.twitter.com/oauth/request_token',
